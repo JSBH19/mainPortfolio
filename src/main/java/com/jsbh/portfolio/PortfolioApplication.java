c506a10e6bc71c3494d633d4e5d47a3fc5762dfa
minje <mingje89@gmail.com>
2019-12-14 15:28:44 +0900

    뭔지모르겠지만 변경사항 있어서 커밋

--- src/main/java/com/jsbh/portfolio/PortfolioApplication.java
+++ src/main/java/com/jsbh/portfolio/PortfolioApplication.java
@@ -1,7 +1,12 @@
 package com.jsbh.portfolio;
 
+import com.jsbh.portfolio.domain.TestTable;
+import com.jsbh.portfolio.repository.TestRepository;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PortfolioApplication {
@@ -10,4 +15,12 @@ public class PortfolioApplication {
 		SpringApplication.run(PortfolioApplication.class, args);
 	}
 
+	@Bean
+    public CommandLineRunner initData(TestRepository repository) {
+        return (args) -> {
+			repository.save(new TestTable("aaa"));
+			repository.save(new TestTable("bbb"));
+			repository.save(new TestTable("ccc"));
+        };
+    }
 }
--- src/main/java/com/jsbh/portfolio/controller/MainController.java
+++ src/main/java/com/jsbh/portfolio/controller/MainController.java
@@ -1,5 +1,8 @@
 package com.jsbh.portfolio.controller;
 
+import com.jsbh.portfolio.service.TestService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,8 +12,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
 
+    @Autowired TestService testService;
+
     @RequestMapping("/")
     public String goingToMain() {
+        return "index";
+    }
+
+    @RequestMapping("/test")
+    public String test() {
+        int count = testService.countAllData();
+        System.out.println("data는 " + count + "개 있습니다");
         return "index";
     }
     
